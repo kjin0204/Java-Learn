0fc0d1ab4aa8bcdb92a18c3728bad21a54fa1754
kang <59352634+kjin0204@users.noreply.github.com>
2026-01-18 17:07:30 +0900

    [level 1] Title: 덧칠하기, Time: 2.19 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -3,39 +3,14 @@ import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        boolean finish;
-                
-        List<Integer> list = Arrays.stream(section)
-                            .boxed()          // int를 Integer로 포장
-                           .collect(Collectors.toList()); // 리스트로 수집
-        
-        paint(list.get(0) , m, list , n );
-        
-        return count;
-    }
-    
-    int count;
-    
-    public void paint(int s, int m , List<Integer> section, int n ) {
-        if(section.size() == 0 ) return;
-        
-        // if(s + m > n) 
-        //     s -= n - (s + m);
-        
-        List<Integer> remain = new ArrayList<>();
-        
-        
-        for(int i = 0; i < section.size(); i++) {
-            if(s + m - 1< section.get(i) || s > section.get(i) )
-                remain.add(section.get(i));
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
+            }
         }
-        
-        count++;
-        
-        if(remain.size() > 0 ) 
-            paint(remain.get(0), m, remain, n);
-        
-        
+        return cnt;
     }
 }
